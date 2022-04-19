df8e68fcc54d72d7852b4dd9f4708fb44ca7db49
scottc11 <scott.m.campbell11@gmail.com>
2022-04-19 16:53:47 -0400

    got websocket conection working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useWebSocket from 'react-use-websocket';
 
 function App() {
+
+  const { sendMessage, lastMessage, readyState, getWebSocket } = useWebSocket(
+    "wss://hometask.eg1236.com/game1/",
+    {
+      share: true,
+      shouldReconnect: () => false,
+    }
+  );
+
+  useEffect(() => {
+    lastMessage && console.log(lastMessage.data);
+  }, [lastMessage]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => sendMessage("help")}>Help</button>
+        <button onClick={() => sendMessage("map")}>Map</button>
+        <button onClick={() => sendMessage("new 1")}>New</button>
+        <button onClick={() => sendMessage("open")}>Open</button>
       </header>
     </div>
   );
