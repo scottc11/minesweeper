1ceb480279ea655668a6497a5dbd32bb63354842
scottc11 <scott.m.campbell11@gmail.com>
2022-04-19 20:25:37 -0400

    hooked up typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import useWebSocket from 'react-use-websocket';
+import { Map } from './components';
 import Button from '@mui/material/Button';
 
 function App() {
@@ -23,7 +24,8 @@ function App() {
         <Button onClick={() => sendMessage("help")}>Help</Button>
         <Button onClick={() => sendMessage("map")}>Map</Button>
         <Button onClick={() => sendMessage("new 1")}>New Game</Button>
-        <Button onClick={() => sendMessage("open")}>Open</Button>
+        <Button onClick={() => sendMessage("open 1 2")}>Open</Button>
+        <Map x={1} y={2} />
       </header>
     </div>
   );
---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+interface MapProps {
+    x: number;
+    y: number;
+}
+
+const Map: FC<MapProps> = (props) => {
+    return (
+        <div>
+            Map
+        </div>
+    )
+}
+
+export default Map;
