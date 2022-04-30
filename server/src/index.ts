2bfa77f23e6da70206db8a376be51f294c890947
scottc11 <scott.m.campbell11@gmail.com>
2022-04-30 15:02:44 -0400

    setup node server

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+/*
+'M' represents an unrevealed mine,
+'E' represents an unrevealed empty square,
+'B' represents a revealed blank square that has no adjacent mines (i.e., above, below, left, right, and all 4 diagonals),
+digit ('1' to '8') represents how many mines are adjacent to this revealed square, and
+'X' represents a revealed mine.
+*/
+
+console.log("Hellooo Minesweeper ⛏");
+
+enum BoardTypes {
+    UNREVEALED_MINE,
+    REVEALED_MINE,
+    EMPTY,
+    BLANK
+}
+
+const board = [["E", "E", "E", "E", "E"], ["E", "E", "M", "E", "E"], ["E", "E", "E", "E", "E"], ["E", "E", "E", "E", "E"]];
