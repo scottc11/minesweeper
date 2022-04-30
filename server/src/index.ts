968002afe1615c0557acff64ac69517cf0b9e73f
scottc11 <scott.m.campbell11@gmail.com>
2022-04-30 18:47:16 -0400

    built tile mine counter

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,4 @@
-
-
-
+const readline = require('node:readline');
 /*
 'M' represents an unrevealed mine,
 'E' represents an unrevealed empty square,
@@ -11,9 +9,10 @@ digit ('1' to '8') represents how many mines are adjacent to this revealed squar
 
 console.log("Hellooo Minesweeper ⛏");
 
-
-
-type TilePosition = [number, number];
+interface TileType {
+    row: number;
+    col: number;
+}
 
 enum TileTypes {
     MINE = "*",
@@ -27,13 +26,15 @@ class Game {
     columns: number;
     numMines: number;
     board: BoardType;           // state of the board
-    mines: Array<TilePosition>; // stores (x,y) coordinates of all mines.
+    boardMask: BoardType;       // what the user sees (ie. state)
+    mines: Array<TileType>; // stores (x,y) coordinates of all mines.
 
     constructor(rows: number, columns: number, numMines: number) {
         this.rows = rows;
         this.columns = columns;
         this.numMines = numMines;
         this.board = new Array();
+        this.boardMask = new Array();
         this.mines = new Array(this.numMines);
     }
 
@@ -48,20 +49,60 @@ class Game {
         this.placeMines();
     }
 
-    randomlyPlaceMine(): TilePosition {
+    randomlyPlaceMine(): TileType {
         
         let random = Math.random() * ((this.rows * this.columns) - 1);
         random = Math.round(random);
         let x: number = Math.floor(random / this.rows);
         let y: number = Math.floor(random % this.columns);
-        return [x, y];
+        return { row: x, col: y };
     }
 
     placeMines() {
         for (let i = 0; i < this.mines.length; i++) {
             this.mines[i] = this.randomlyPlaceMine();
-            this.board[this.mines[i][0]][this.mines[i][1]] = TileTypes.MINE;
+            this.board[this.mines[i].row][this.mines[i].col] = TileTypes.MINE;
         }
+    }
+
+    // count all the mines surrounding the given tile position
+    countNeighboringMines(tile: TileType): number {
+        /*
+            N.W  N   N.E
+              \  |  /
+            W-- Cell -- E
+              /  |  \
+            S.W  S   S.E
+        */
+        const positions = [
+            [-1, 0], // North
+            [-1, 1], // N.E
+            [0, 1],  // East
+            [1, 1],  // S.E
+            [1, 0],  // South
+            [1, -1], // S.W
+            [0, -1], // West
+            [-1, -1] // N.W
+        ]
+        let counter: number = 0;
+        for (let i = 0; i < positions.length; i++) {
+            let ghostTile = { row: tile.row + positions[i][0], col: tile.col + positions[i][1] };
+            if (this.tileExists(ghostTile)) {
+                if (this.tileIsMine(ghostTile)) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+    
+    tileExists(tile: TileType): boolean {
+        return (tile.row >= 0) && (tile.row < this.rows) && (tile.col >= 0) && (tile.col < this.columns);
+    }
+
+    // check if given tile is a mine
+    tileIsMine(tile: TileType): boolean {
+        return this.board[tile.row][tile.col] === TileTypes.MINE ? true : false;
     }
 }
 
@@ -69,3 +110,17 @@ const game: Game = new Game(10, 10, 25);
 
 game.reset();
 console.table(game.board);
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+rl.question("Enter Coordinate: (ex. 'row col')", function (position: string) {
+    let coords = position.split(" ");
+    let row: number = Number(coords[0]);
+    let col: number = Number(coords[1]);
+    console.log(game.countNeighboringMines({ row, col }));
+});
+
+rl.on("close", function () {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+});
