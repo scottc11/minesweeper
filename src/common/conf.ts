664a81cdbea765d5a3547add0b858053ccefbf03
scottc11 <scott.m.campbell11@gmail.com>
2022-05-11 12:39:21 -0400

    changed server url path

--- src/common/conf.ts
+++ src/common/conf.ts
@@ -1,3 +1,3 @@
 export const ServerPort: number = Number(process.env.PORT) || 8000;
 
-export const ServerURL: string = `http://localhost:${ServerPort}/`;
+export const ServerURL: string = `http://3.80.49.79:${ServerPort}/`;
